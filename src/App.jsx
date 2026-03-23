1ec9f4d4a81d7a59617929d0fb67f563eb68688d
northlandcheese <125759958+northlandcheese@users.noreply.github.com>
2026-03-23 14:47:06 +0800

    yo

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <main className="only-text" aria-label="Chūsha theclib at">
+      蟲社theclib!dddssss
+    </main>
+  )
+}
+
+export default App
